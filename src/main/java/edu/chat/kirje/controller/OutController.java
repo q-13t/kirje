0051aebeaa3fe4dfe2efb16c900d0aab33fe75b5
q-13t <vova29092002@gmail.com>
2023-12-06 16:23:01 +0200

    IMP: UDPConnector [BASE]

--- src/main/java/edu/chat/kirje/controller/OutController.java
+++ src/main/java/edu/chat/kirje/controller/OutController.java
@@ -1,18 +1,24 @@
 package edu.chat.kirje.controller;
 
-import org.springframework.ui.Model;
-
+import java.io.IOException;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.multipart.MultipartFile;
-import org.springframework.web.bind.annotation.RequestPart;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestPart;
+import org.springframework.web.multipart.MultipartFile;
+
+import edu.chat.kirje.UDPConnector.UDPOperator;
 
 @Controller
 public class OutController {
+
+    @Autowired
+    private UDPOperator operator;
 
     @RequestMapping(path = "/", method = RequestMethod.GET)
     public String testMapping(Model model) {
@@ -28,8 +34,13 @@ public class OutController {
             System.out.println("Received file: " + files.get(index).getOriginalFilename());
         }
         System.out.println("Received text: " + textAreaInput);
+        try {
+            operator.sendMessage(textAreaInput);
+            return ResponseEntity.ok("OK");
 
-        // Return a response
-        return ResponseEntity.ok("OK");
+        } catch (IOException e) {
+            e.printStackTrace();
+            return ResponseEntity.ok("NOT OK");
+        }
     }
 }
